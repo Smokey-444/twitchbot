c1bf8e3115f1dd07e11f4e3592db963ffe0954fa
Smokey-444 <85306561+Smokey-444@users.noreply.github.com>
2021-07-04 19:23:17 +0200

    1

---
+++ index.js
@@ -0,0 +1,51 @@
+const tmi = require('tmi.js')
+
+const options = {
+    options: {
+        debug: true
+    },
+    connection: {
+        cluster: 'aws',
+        reconnect: true,
+    },
+    identity: {
+        username: 'lemaxxxxxxxbot',
+        password: 'oauth:jsjuxjbqfa4xqhyi3d89g5beatetw6'
+    },
+    channels: ['lemaxxxxxxx']
+}
+
+const client = new tmi.client(options);
+
+client.connect();
+
+client.on('connected', (address, port) => {
+    console.log('Verbindungsaufbau Erfolgreich')
+})
+
+client.on('chat', (channel, user, message, self) => {
+    if(message === '!help' || message === '!Help' || message === '!hilfe' || message === '!Hilfe') {
+        client.action('lemaxxxxxxx', 'Hi')
+    }
+    if(message === '!hi' || message === '!hey' || message === 'hi' || message === 'hey') {
+        client.action('lemaxxxxxxx', `Heyy, @${user.username}`)
+    }
+    // Kannst auch noch andere Befehle hinzufügen :D
+})
+
+client.on('subscription', (channel, username, method, message, userstate) => {
+    console.log('subscription', { channel, username, method, message, userstate })
+    client.say('lemaxxxxxxx', `Danke @${username}, dass du gerade gesubbt hast!`)
+})
+
+client.on('resub', (channel, username, _months, message, userstate, methods) => {
+    console.log('resub', { channel, username, _months, message, userstate, methods })
+    let streakmonths = userstate['msg-param-streak-months'];
+    let cumulativeMonths = userstate['msg-param-cumulative-months']
+    let sharedStreak = userstate['msg-param-should-share-streak']
+    if(sharedStreak) {
+        client.say('lemaxxxxxxx', `Danke, dass du durchgängig für ${streakmonths} konstante Monate gesubbt hast! @${username}`)
+    } else {
+        client.say('lemaxxxxxxx', `Danke dass du resubst, warst jz insgesamt ${cumulativeMonths} Monate Sub :D @${username}`)
+    }
+})
