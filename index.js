584a6979b549360266d5c0f4c407602b48d5b299
Smokey-444 <85306561+Smokey-444@users.noreply.github.com>
2021-07-04 19:41:33 +0200

    5

--- index.js
+++ index.js
@@ -51,15 +51,7 @@ client.on('resub', (channel, username, _months, message, userstate, methods) => 
     } else {
         client.say('lemaxxxxxxx', `Danke dass du resubst, warst jz insgesamt ${cumulativeMonths} Monate Sub :D @${username}`)
     }
-   if(parts[0] == '!clear' || parts[0] == 't!purge') {
-        if(!message.member.hasPermission('MANAGE_MESSAGES')) return message.channel.send('Du brauchst die Berechtigung, Nachrichten zu löschen!')
-        if(!parts[1]) return message.channel.send('Du musst angeben, wieviele Nachrichten du löschen möchtest!')
-        if(isNaN(parts[1])) return message.channel.send('Die Angabe, wieviele Nachrichten du löschen möchtest, muss eine Zahl sein!')
-        if(parts[1] > 100) return message.channel.send('Du kannst nicht mehr als 100 Nachrichten löschen!')
-        if(parts[1] < 2) return message.channel.send('Du kannst nicht weniger als 2 Nachricht löschen')
-        message.channel.bulkDelete(parts[1])
-        message.channel.send(`Ich habe erfolgreich **${parts[1]}** Nachrichten gelöscht!`).then(m => m.delete({timeout: 3000}))
-    }
+    
 })
 
         
